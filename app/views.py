6cf5fd9fc2b3bd8435bd54d1ff5b53512be1252e
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-10 23:30:18 +0300

    add a view function that will take dynamic argument value

--- app/views.py
+++ app/views.py
@@ -7,4 +7,12 @@ def index():
   '''
   Index page view function that returns the index page and its data
   '''
-  return render_template('index.html')
+  message = 'hello world'
+  return render_template('index.html',message = message)
+
+@app.route('new/<new_id>')
+def new(new_id):
+  '''
+  news view page function that returns the movie details page and its data
+  '''
+  return render_template('new.html', id = new_id)
