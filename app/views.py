a30dd58cb7dd8634a41b4cfdc5e44017405aec0e
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-11 23:18:37 +0300

    Add view function to display news source

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news_source
 
 # Views
 @app.route('/')
@@ -7,12 +8,16 @@ def index():
   '''
   Index page view function that returns the index page and its data
   '''
-  message = 'hello world'
-  return render_template('index.html',message = message)
 
-@app.route('new/<new_id>')
+  newsSource = get_news_source()
+  # top_headlines = get_top_headlines()
+  title = 'Home - Welcome to the News website'
+  return render_template('index.html',title=title, source = newsSource)
+
+@app.route('/new/<int:new_id>')
 def new(new_id):
   '''
   news view page function that returns the movie details page and its data
   '''
+  title = f'You are viewing {{new_id}}'
   return render_template('new.html', id = new_id)
