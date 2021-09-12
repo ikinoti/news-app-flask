a4eb1ec60096bc68f010e06054130cc8ccadc752
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-09-12 11:21:14 +0300

    created our app factory function

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask
-from .config import DevConfig
+from flask_bootstrap import Bootstrap
+from config import config_options
 
-# Initializing application
-app = Flask(__name__, instance_relative_config=True)
-# setting up configutation
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+bootstrap = Bootstrap
 
-from app import views
+def create_app(config_name):
+
+  
+  app = Flask(__name__)
+
+  # creating the app configutation
+  app.config.from_object(config_options[config_name])
+  
+  #  initializing flask extension
+  bootstrap.init_app(app)
+
+  
+
+  return app
